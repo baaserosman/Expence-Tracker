e3ac45ad93e4ef7904b38a6c98501f946fa94c74
baaserosman <baaser.osman@gmail.com>
2022-04-04 00:19:40 +0200

    form onSubmit added

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -20,16 +20,17 @@ const AddTransaction = () => {
   return (
     <div>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
-            onChange={(e) => setText(e.target.value)}
             type="text"
+            name="text"
             id="text"
             required
             value={text}
             placeholder="Enter text..."
+            onChange={(e) => setText(e.target.value)}
           />
         </div>
         <div className="form-control">
@@ -38,15 +39,15 @@ const AddTransaction = () => {
             (negative - expense, positive - income)
           </label>
           <input
-            onChange={(e) => setAmount(e.target.value)}
             type="number"
             id="amount"
             required
             value={amount}
             placeholder="Enter amount..."
+            onChange={(e) => setAmount(e.target.value)}
           />
         </div>
-        <button type="submit" onClick={handleSubmit} className="btn">
+        <button type="submit" className="btn">
           Add transaction
         </button>
       </form>
